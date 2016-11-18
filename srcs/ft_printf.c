5cc947c5cb033ffeeef949a63e73c0a17025ee02
etrobert <etiennerobert33@gmail.com>
2016-11-18 15:09:17 +0100

    Avancees

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -1,5 +1,26 @@
 #include "libft.h"
 
+/*
+** Etape I : Construction de la liste d'elems
+** Chaque elem correspond a une portion de la chaine finale
+** Ex : ft_printf("J'ai %d ans.", 42);
+** se transforme en trois elem "J'ai " "42" et " ans."
+**
+** Etape II : Concatenation des elems entre eux
+** POur ce faire la fonction de concatenation va d'abord devoir malloc
+** Une grande chaine de characteres. POur ce faire, la taille finale est mise
+** a jour a chaque ajout de elem lors de la construction de la liste
+*/
+
+typedef struct	elem
+{
+	char	*str;
+	size_t	size;
+	t_elem	*next;
+}				t_elem;
+
+char	*concatenate(t_elem *first);
+
 int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
